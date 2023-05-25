ab17622feab4380fe5020f4d638c73cd6dbd2817
MrBabyBlack <114551576+MrBabyBlack@users.noreply.github.com>
2023-05-25 09:55:01 +0700

    Delete week1 directory

--- week1/coba1.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    define('PHI', 3.14);
-    define('DBNAME', 'Inventori');
-    define('DBSERVER', 'Localhost');
-
-    echo 'Nilai phi : ' . PHI;
-    echo '<br> Nama database nya : ' . DBNAME;
-    echo '<br> Lokasi Database nya : ' . DBSERVER;
